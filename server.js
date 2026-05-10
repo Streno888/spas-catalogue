0c502c5c422137077807cc048712f1d89c9a90ab
Streno888 <donsretno@gmail.com>
2026-05-10 14:17:22 +0300

    Initial catalogue deploy

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/catalogue-v2-updated.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`SPAS catalogue running on port ${PORT}`);
+});
